89cfbd6894e02ef165398ecaff6cbc192251005e
Manish Adhikari <manishadhikari123@gmail.com>
2024-05-05 22:09:49 +0100

    refactor: move helm charts to k8s directory

--- k8s/init.bat
+++ k8s/init.bat
@@ -2,6 +2,7 @@
 SET clusterType=k8s/multiNodeClusterConfig.yaml
 SET clusterName=my-k8s-kind-cluster
 SET appNamespace=dictionary-service-ns
+SET dbNamespace=database-ns
 SET appImage=dictionary-service
 SET appImageTag=latest
 SET mongoImage=mongo
@@ -12,7 +13,7 @@ kind create cluster -n %clusterName% --config %clusterType%
 echo "======== Kubernetes dashboard is installing.. ========"
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
-helm install k8s-dashboard-rel ./k8s-dashboard-helm-chart
+helm install k8s-dashboard-rel k8s/k8s-dashboard-helm-chart
 echo "======== Waiting for kubernetes dashboard to become available.. ======== "
 timeout 30
 
@@ -40,11 +41,14 @@ kubectl patch deployment metrics-server -n kube-system --patch-file k8s/patch-me
 echo "======== Waiting for metrics server to become available.. ======== "
 timeout 30
 
-for /F %%v in ('kubectl config current-context') do @SET "currContext=%%v"
-echo "Current context is = %currContext%"
-kubectl create ns %appNamespace%
-kubectl config set-context %currContext% --namespace %appNamespace%
-kubectl config use-context %currContext% --namespace %appNamespace%
+kubectl create ns %dbNamespace%
+kubectl label ns %dbNamespace% ns-type=db-ns
+echo "======== Installing mongodb helm chart.. ========"
+::to install mongodb in controlplane node
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+helm install mongodb-rel k8s/mongodb-helm-chart
+echo "======== Waiting for mongodb deployment to become available.. ======== "
+timeout 30
 
 podman build -t docker.io/library/%appImage%:%appImageTag% -f Dockerfile
 podman save docker.io/library/%appImage%:%appImageTag% -o target/%appImage%-%appImageTag%.tar
@@ -53,13 +57,15 @@ podman pull docker.io/library/%mongoImage%:%mongoImageTag%
 podman save docker.io/library/%mongoImage%:%mongoImageTag% -o target/%mongoImage%-%mongoImageTag%.tar
 kind load image-archive target/%mongoImage%-%mongoImageTag%.tar --name %clusterName%
 
-echo "======== Installing mongodb helm chart.. ========"
-kubectl taint nodes --all node-role.kubernetes.io/control-plane-
-helm install mongodb-rel ./mongodb-helm-chart
-echo "======== Waiting for mongodb deployment to become available.. ======== "
-timeout 30
+kubectl create ns %appNamespace%
+kubectl label ns %appNamespace% ns-type=app-ns
 echo "======== Installing dictionary-service helm chart.. ========"
-helm install dictionary-rel ./dictionary-service-helm-chart
+helm install dictionary-rel k8s/dictionary-service-helm-chart
+
+for /F %%v in ('kubectl config current-context') do @SET "currContext=%%v"
+echo "Current context is = %currContext%"
+kubectl config set-context %currContext% --namespace %appNamespace%
+kubectl config use-context %currContext% --namespace %appNamespace%
 
 ::helm uninstall dictionary-rel
 ::helm uninstall mongodb-rel
--- src/main/java/com/learn/microservice/controller/DictionaryErrorController.java
+++ src/main/java/com/learn/microservice/controller/DictionaryErrorController.java
@@ -1,15 +1,18 @@
 package com.learn.microservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class DictionaryErrorController implements ErrorController {
 
     @GetMapping("/error")
     public ResponseEntity<String> errorMessage(){
+        log.error("Technical error occurred");
         return ResponseEntity.ok("Sorry! It appears that some problem occurred. Please try after sometime");
     }
 }
